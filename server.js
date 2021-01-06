67ee9adc465b6418500ddf91e5eb596e0ee3eff7
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-01-06 23:32:46 +0530

    cors add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000 ; 
 app.use(express.static('public'));
@@ -9,6 +9,11 @@ app.use(express.json());
 const connectDB = require('./config/db')
 connectDB();
 
+//Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions))
 //Template engine
 
 app.set('views', path.join(__dirname,'/views'));
