191f9c3a974aa4f145af8633d70ab948eab34dab
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-01-07 08:34:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.static('public'));
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 const connectDB = require('./config/db');
 connectDB();
 
