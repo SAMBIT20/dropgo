f59eccec4dc6fd26ad23c6920d51fc7043ceaeb1
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-01-07 08:22:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-}
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(',')
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
