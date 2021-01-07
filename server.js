40a0904374c2268db51fa020153c733bd940c244
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-01-07 08:17:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
 
-const PORT = process.env.PORT || 3000 ; 
+app.use(cors(corsOptions))
 app.use(express.static('public'));
-app.use(express.json());
-const connectDB = require('./config/db')
+
+const connectDB = require('./config/db');
 connectDB();
 
-//Cors
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOptions))
-//Template engine
+app.use(express.json());
 
-app.set('views', path.join(__dirname,'/views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
-//Routes
-app.use('/api/files', require('./routes/files'))
-app.use('/files', require('./routes/show'))
-app.use('/files/download',require('./routes/download'))
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
 
 
-app.listen(PORT, ()=>{
-    console.log(`Listining on port ${PORT}`);
-})
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
